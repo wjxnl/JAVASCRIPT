b48b45288698e782e2dd15806bab554005733435
wjxnl <079bei008@ioepc.edu.np>
2024-12-05 22:44:50 +0545

    event file updated

---
+++ events.js
@@ -0,0 +1,7 @@
+//Event: The change in the state of an object is Event.
+//event handling in js
+let btn1 = document.querySelector("#btn1");
+btn1.onclick= () => {
+    console.log("button clicked.");
+    
+}
